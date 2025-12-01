b2ad858de91735c4c2d8cb448bd3a72123c79139
Emily Prieto <61399347+stygian-ash@users.noreply.github.com>
2025-12-01 10:59:14 -0500

    feat: Add organization card to event info screen

--- src/app/(tabs)/(feed,myRSVPs)/event/[id].tsx
+++ src/app/(tabs)/(feed,myRSVPs)/event/[id].tsx
@@ -1,6 +1,7 @@
 import EventInfo from "@/src/components/EventInfo";
 import GVArea from "@/src/components/GVArea";
 import NavigationBar from "@/src/components/NavigationBar";
+import OrganizationCard from "@/src/components/OrganizationCard";
 import supabase from "@/src/utils/requests";
 import {storage} from "@/src/utils/storage";
 import {useGlobalSearchParams} from "expo-router";
@@ -11,6 +12,7 @@ export default function EventInfoPage() {
     const [event, setEvent] = useState()
     const [userType, setUserType] = useState<string>()
     const {id} = useGlobalSearchParams()
+    const organization = event?.users?.organizations
 
     const getUserType = async () => {
         const userType = await storage.get('userType');
@@ -19,15 +21,15 @@ export default function EventInfoPage() {
     useEffect(() => void getUserType, [userType])
 
     const fetchEventInfo = async () => {
-        const {data: eventData, error: eventError} = await supabase.from('events')
-            .select('*, categories (*)')
+        const {data, error} = await supabase.from('events')
+            .select('*, categories (*), users!events_organization_id_fkey (email, organizations (*))')
             .eq('id', id)
-        if (eventError != null || eventData.length < 1) {
-            console.error(`/event/${id} -- Failed to fetch event info:`, eventError)
+        if (error != null || data.length < 1) {
+            console.error(`/event/${id} -- Failed to fetch event info:`, error)
             return
         }
-        setEvent(eventData[0])
-        console.log(`/event/${id} -- Got data`, eventData[0])
+        setEvent(data[0])
+        console.log(`/event/${id} -- Got data`, data[0])
     }
 
     useEffect(() => void fetchEventInfo(), [])
@@ -51,7 +53,17 @@ export default function EventInfoPage() {
                             state={event.state}
                             imageURI={event.image_url}
                             categoryName={event.categories?.name}
-                    />
+                        />
+                }
+                {organization
+                    && <OrganizationCard
+                            title={organization.title}
+                            phone={organization.phone}
+                            email={event.users.email}
+                            organizationURL={organization.organization_url}
+                            city={organization.city}
+                            state={organization.state}
+                        />
                 }
             </ScrollView>
         </GVArea>
